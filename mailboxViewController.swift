ea3c7045a85c9313f608282fa6aed53aec8f9f61
chrislawcodes <chrislaw@gmail.com>
2015-10-02 16:05:57 -0700

    Reschedule Image showing!

--- mailboxViewController.swift
+++ mailboxViewController.swift
@@ -14,8 +14,20 @@ class mailboxViewController: UIViewController {
 
     @IBOutlet weak var singleMessageUIView: UIImageView!
     
-    var msgOriginalCenter: CGPoint!
+    @IBOutlet weak var laterUIImageView: UIImageView!
+    
+    @IBOutlet weak var containerMessageView: UIView!
+    
+    @IBOutlet weak var rescheduleImageView: UIImageView!
+    
     var initialMsgCenter: CGPoint!
+    var msgOriginalCenter: CGPoint!
+    
+    var initialLaterCenter: CGPoint!
+    var laterOriginalCenter: CGPoint!
+    
+    
+    
 
     
     override func viewDidLoad() {
@@ -23,7 +35,10 @@ class mailboxViewController: UIViewController {
           mailScrollView.contentSize = CGSize(width: 375, height: 1400)
         var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onCustomPan:")
           singleMessageUIView.addGestureRecognizer(panGestureRecognizer)
+
         initialMsgCenter = singleMessageUIView.center
+        initialLaterCenter = laterUIImageView.center
+        laterUIImageView.alpha = 0.25
 
         
         // Do any additional setup after loading the view.
@@ -41,14 +56,37 @@ class mailboxViewController: UIViewController {
         
         if panGestureRecognizer.state == UIGestureRecognizerState.Began {
             msgOriginalCenter = singleMessageUIView.center
+            laterOriginalCenter = laterUIImageView.center
             print("Gesture began at: \(point)")
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
-            // check to see if message is being dragged to the left.
-            
-            
-            
+            // move the message and show that we tracked it
             singleMessageUIView.center = CGPoint(x: msgOriginalCenter.x + translation.x, y: msgOriginalCenter.y)
             print("Gesture changed at: \(point)")
+
+            // check to see if message is between 0 and 60 pixels
+            if (singleMessageUIView.center.x < initialMsgCenter.x) && (singleMessageUIView.center.x > initialMsgCenter.x - 60){
+                // make icon opaque
+                UIView.animateWithDuration(0.5, animations:{
+                    self.laterUIImageView.alpha = 1
+
+                    })
+                
+                print("I'm in between 0 and 60")
+                
+            }
+
+                // check to see if message is being dragged to the left between 60 and 260 pixels
+            else if (singleMessageUIView.center.x < initialMsgCenter.x - 61) && (singleMessageUIView.center.x > initialMsgCenter.x - 260){
+                // move icon with the view & make background Yellow
+                print("I'm between 61 and 260")
+                UIView.animateWithDuration(0.5, animations:{
+                self.containerMessageView.backgroundColor = .yellowColor()
+                })
+                self.laterUIImageView.center.x = laterOriginalCenter.x + translation.x + 60
+
+            }
+
+            
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
             // check to see if message was moved left or right more than 60 pixels
             
@@ -56,8 +94,16 @@ class mailboxViewController: UIViewController {
                 //snap the message back
                 UIView.animateWithDuration(0.4, animations: {
                     self.singleMessageUIView.center.x = self.initialMsgCenter.x
-                        print("Gesture ended at: \(point)")
+                    self.containerMessageView.backgroundColor = .grayColor()
+                    self.laterUIImageView.alpha = 0.25
+                    print("Gesture ended at: \(point)")
                 })
+            }
+                // check to see if between 61 and 260 pixels
+                else if (singleMessageUIView.center.x < initialMsgCenter.x - 61) && (singleMessageUIView.center.x > initialMsgCenter.x - 260){
+                    UIView.animateWithDuration(0.5, animations:{
+                        self.rescheduleImageView.alpha = 1
+                    })
             }
         }
     
